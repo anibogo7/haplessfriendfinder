647e913571bde5b5c30f9bb7ca267c1e1d1b2305
Ani <a.bogosian7@gmail.com>
2019-03-19 00:06:28 -0700

    reinstall restoation

---
+++ README.md
@@ -0,0 +1,31 @@
+# Hapless Friend Finder
+
+
+## Description
+
+An app to find you a friend that is psychologically copatible.   Take the quiz and see who best suites you.
+
+
+*Hapless Friend Finder* implements friend matching based on the user's responses to a ten question survey. The user responds to questions with values from 1 (Strongly Disagree) to 5 (Strongly Agree). When the survey is submitted, an existing user record closest to the current user's responses is found and returned. The closest set of user responses is defined as the set with the lowest absolute difference for all ten questions combined.
+
+*Hapless Friend Finder* application is meant to simulate a simple dating app. The application is implemented using a [Node.js](https://nodejs.org/en/) and [Express](https://expressjs.com/) server on the back end and the [Materialize](http://materializecss.com/) CSS framework on the front end.
+
+## Demo
+	
+*Friend Finder* is deployed to Heroku. Please check it out [here](https://fierce-fortress-60021.herokuapp.com/).
+
+## Installation
+
+To install the application follow the instructions below:
+
+	git clone git@github.com:angrbrd/friend-finder.git
+	cd friend-finder
+	npm install
+	
+## Running Locally
+
+To run the application locally and access it in your browser, run the Node.js application with the command below.
+
+	node server.js
+	
+The application will now be running locally on `PORT`, Usually localhost:8080 but may be different. You can then access it locally from your browser at the URL, in this case `localhost:8080`.
---
+++ app/data/friends.js
@@ -0,0 +1,236 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Jeffrey",
+    photo: "dathttps://pbs.twimg.com/profile_images/975012794268516352/vWsUzdma_400x400.jpg",
+    scores: [
+      "5",
+      "2",
+      "4",
+      "4",
+      "5",
+      "5",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Pablo",
+    photo: "http://www.manthaphillips.ca/wp-content/uploads/2017/03/Jean-Charles-reduced.jpg",
+    scores: [
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: "Thomas",
+    photo: "http://www.manthaphillips.ca/wp-content/uploads/2017/03/Jean-Charles-reduced.jpg",
+    scores: [
+      "2",
+      "2",
+      "2",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Ganjib",
+    photo: "https://www.nic.in/wp-content/uploads/2018/06/PM.jpg",
+    scores: [
+      "3",
+      "3",
+      "1",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "4"
+    ]
+  },
+  {
+    name: "Steven",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBtwE98huAX1LXXV3WTpjjJDHLbeFP0yYvZHITgWOYosbx50Pe",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "4",
+      "2",
+      "5",
+      "3",
+      "5",
+      "3"
+    ]
+  },
+  {
+    name: "Alan",
+    photo: "http://www.stalingrad-battle.ru/images/stories/sveta/vasinav.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "2",
+      "2",
+      "2",
+      "1",
+      "3",
+      "1"
+    ]
+  },
+  {
+    name: "Louis",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT34_1Amup6v5jTziX3mtWZNvJgLDbIieBhew74kRWqpJmNejBC",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "4",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Zachary",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCEHKL1eBxClIUFWIbkdAdvu49o_J54WLMagtV-cJ_-yczR4xGcA",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "5",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Winston",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-6vtT3ClaZb-LYFNvx2aS2_8Ep48KAfGPQdyFLfjmWUZ470XF",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Derrick",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEs6vzhRcA83N8d7hPb3dsZ8ouGkMd8VDkH6V0we5-VTE36XLM",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "5",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Chester",
+    photo: "https://thoughtcatalog.files.wordpress.com/2016/08/photo-1441622915984-05d13dfb3d8c.jpg?w=1140&h=760",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Owen",
+    photo: "https://www.muktiorganics.com/img/uploaded2015/good-skin.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Michael",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUspb_YiRH1rRAp_axlRq3wlKOlAZ0oSMsQIyNetEHfOecAbTBvQ",
+    scores: [
+      "1",
+      "1",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "1",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Frank",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs4KUQ3WpROhZ8oC_IQjGxTVIrFhakbJr-EwrtpsaDhlZgNDbNCw",
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
